29c84c1170e6d3d3ec421168c3c1b311436aa647
Simonas Janulaitis <136367934+SimasCode@users.noreply.github.com>
2023-08-28 11:23:45 +0300

    onAuthStateChanged

--- src/store/AuthProvider.jsx
+++ src/store/AuthProvider.jsx
@@ -1,4 +1,6 @@
-import { createContext, useContext, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { auth } from '../firebase/firebase';
 
 const AuthContext = createContext({
   userEmail: null,
@@ -6,6 +8,7 @@ const AuthContext = createContext({
 });
 
 export default function AuthProvider(props) {
+  const [fireUser, setFireUser] = useState({});
   const [userEmail, setUserEmail] = useState('');
 
   function login(gotEmail) {
@@ -16,6 +19,26 @@ export default function AuthProvider(props) {
     email: userEmail,
     login: login,
   };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        // ...
+        console.log('Prisijungta');
+        console.log('user ===', user);
+        setFireUser(user);
+      } else {
+        // User is signed out
+        // ...
+        console.log('Atsijungta');
+        setFireUser(null); //arba tuscias objektas {}
+      }
+    });
+  }, []);
+
   return (
     <AuthContext.Provider value={ctx}>{props.children}</AuthContext.Provider>
   );
