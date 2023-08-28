833433582f07e72bd0c9c9f8dbeb05bc20c78076
Simonas Janulaitis <136367934+SimasCode@users.noreply.github.com>
2023-08-28 11:16:57 +0300

    isLoggedIn

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,12 @@
 import { Link, NavLink } from 'react-router-dom';
 import './Header.scss';
+import { useAuth } from '../../store/AuthProvider';
 
 export default function Header() {
+  const ctx = useAuth();
+  console.log('ctx ===', ctx);
+
+  const isLoggedIn = false;
   return (
     <header className='header'>
       <div className='container'>
@@ -12,12 +17,21 @@ export default function Header() {
           <NavLink className={'navLink'} to={'/'}>
             Home
           </NavLink>
-          <NavLink className={'navLink'} to={'/profile'}>
-            My Profile
-          </NavLink>
-          <NavLink className={'navLink'} to={'/login'}>
-            Login
-          </NavLink>
+          {isLoggedIn && (
+            <NavLink className={'navLink'} to={'/profile'}>
+              My Profile
+            </NavLink>
+          )}
+          {!isLoggedIn && (
+            <NavLink className={'navLink'} to={'/login'}>
+              Login
+            </NavLink>
+          )}
+          {isLoggedIn && (
+            <NavLink className={'navLink'} to={'/login'}>
+              Logout
+            </NavLink>
+          )}
         </nav>
       </div>
     </header>
